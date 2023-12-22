edf51f970eacc3da6cba5c6d504d5010234ccf0c
Arjun Arasu <arjunbalan1994@gmail.com>
2023-12-22 17:10:17 +0530

    fa links fixed

---
+++ Build-a-Personal_Portfolio_Webpage/index.js
@@ -0,0 +1,8 @@
+const myMail = document.getElementById('mail-div')
+const myPhone = document.getElementById('call-div')
+myMail.addEventListener('click',function (params) {
+    alert("arjunbalan1994@gmail.com")
+})
+myPhone.addEventListener('click',function (params) {
+    alert("You can call me anytime @ +91 8870122255")
+})
